aba743043db6c136bb8b622f5ace07260a3a515c
abura <hanagevara@gmail.com>
2019-09-23 09:06:20 +0000

    authentication

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,9 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
   end
   
   def show
@@ -13,6 +14,7 @@ class TasksController < ApplicationController
   end
   
   def create
+    # user_idにログインユーザのidをキチンとセットしないといけない。
     @task = Task.new(task_params)
     
     if @task.save
@@ -46,12 +48,15 @@ class TasksController < ApplicationController
 
   private
   
-  def set_task
-    @task = Task.find(params[:id])
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
   
   def task_params
-    params.require(:task).permit(:content, :status)
+    params.require(:task).permit(:content, :status).merge(user_id: current_user.id)
   end
 
 end
